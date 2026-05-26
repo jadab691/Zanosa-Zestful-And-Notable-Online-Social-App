d2d25b25b8b38d51aa8bbd5fb0580972b881e7e9
jadab <sarkarjadab83@gmail.com>
2026-05-27 04:33:12 +0600

    edited mail host in local and added logger and debug

--- BACKEND/utils/mailer.js
+++ BACKEND/utils/mailer.js
@@ -12,6 +12,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  logger: true,
+  debug: true,
 });
 
 export const sendEmail = async ({ to, subject, text, html }) => {
